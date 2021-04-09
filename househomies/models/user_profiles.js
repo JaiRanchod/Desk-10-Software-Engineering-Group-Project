e59cc0e32e8ea98becefb00a5727b7cc8063ec71
JaiRanchod <73884031+JaiRanchod@users.noreply.github.com>
2021-04-09 14:56:51 +0100

    Started Schema

---
+++ househomies/models/user_profiles.js
@@ -0,0 +1,20 @@
+var mongoose = require('mongoose');
+
+var userProfiles = new mongoose.Schema({
+    firstName: { type: String, required: true},
+    lastName: { type: String, required: true },
+    emailUser: { type: String, required: true, unique: true},
+    password: { type: String, required: true},
+    preferredName: { type: String, required: true},
+    gender: { type: String, enum: ["Male", "Female", "Other"], required: true},
+    //Validation check needed to ensure they are at least 18
+    age: { type: Number, required: true},
+    location: { type: String, enum: ["City Centre", "Clifton"], required: true},
+    //Not sure if we need more religions?
+    religion: { type: String, enum: ["Christian", "Hindu", "Muslim", "Buddhist", "Jew"], required: true},
+    //Need to check this one - Not sure if country code stores the '+' 
+    phoneNumber: { type: Number, required: true},
+});
+
+//Creating a model from schema we've just made, and exporting it to be used elsewhere
+//module.exports = mongoose.model('Users', userProfiles);
