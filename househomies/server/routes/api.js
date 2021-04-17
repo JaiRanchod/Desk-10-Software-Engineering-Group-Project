9ab74d163277d9e6fe1409e5f6ee1ceb123ed27e
JaiRanchod <73884031+JaiRanchod@users.noreply.github.com>
2021-04-17 09:21:27 +0100

    Pulling mongo into API and added data service

--- househomies/server/routes/api.js
+++ househomies/server/routes/api.js
@@ -1,9 +1,34 @@
 const express = require('express');
 const router = express.Router();
 
+const userLogins = require('../../models/user_logins');
+const userProfiles = require('../../models/user_profiles');
+
 /* GET api listing. */
 router.get('/', (req, res) => {
   res.send('api works');
+});
+
+router.get('/UserLogins', (req, res) => {
+  const loginInfo = req.body;
+  userLogins.find(loginInfo, (err, data) => {
+    if(err){
+      res.status(500).send(err);
+    } else {
+      res.status(201).send(data);
+    }
+  });
+});
+
+router.get('/UserProfiles', (req, res) => {
+  const userInfo = req.body;
+  userProfiles.find(userInfo, (err, data) => {
+    if(err){
+      res.status(500).send(err);
+    } else {
+      res.status(201).send(data);
+    }
+  });
 });
 
 module.exports = router;
