1848b113a9153c2020acd280b8a1a051ad8dd1d4
JJPears <78802714+JJPears@users.noreply.github.com>
2021-05-03 10:56:20 +0100

    Delete househomies directory

--- househomies/models/user_logins.js
+++
@@ -1,8 +0,0 @@
-var mongoose = require('mongoose');
-
-var userLoginSchema = new mongoose.Schema({
-    email: { type: String, required: true, unique: true},
-    password: { type: String, required: true}
-});
-
-module.exports = mongoose.model('userLogins', userLoginSchema, 'Users Login Details');
